48fc182c212876fe304cb986f3a90ebbe3e50f6b
Christopher Richmond <lancer1977@gmail.com>
2023-02-11 11:21:59 -0500

    commit

---
+++ repo/Program.cs
@@ -0,0 +1,11 @@
+﻿ 
+
+using System.Diagnostics;
+using InterfacesToTS;
+
+var path = ".\\src";
+Console.WriteLine("Hello, World!");
+TypeScriptInterfacesExtension.GenerateTypeScriptInterfaces(path);
+Process.Start("explorer.exe",Directory.GetCurrentDirectory()+ path);
+
+
